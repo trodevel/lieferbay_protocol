acd70e40eff8faf3733902c8c4f80bea8e35584c
trodevel <trodevel@gmail.com>
2019-11-16 03:23:58 +0100

    moved web message into a separate file

--- protocol.h
+++ protocol.h
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 12384 $ $Date:: 2019-11-14 #$ $Author: serge $
+// $Revision: 12387 $ $Date:: 2019-11-16 #$ $Author: serge $
 
 #ifndef LIB_LIEFERBAY_PROTOCOL__PROTOCOL_H
 #define LIB_LIEFERBAY_PROTOCOL__PROTOCOL_H
@@ -70,6 +70,13 @@ struct GeoPosition
     uint32_t        plz;
     double          latitude;
     double          longitude;
+};
+
+struct RideSummary
+{
+    GeoPosition         position;
+    basic_objects::LocalTime delivery_time;
+    double              max_weight;
 };
 
 struct Offer
@@ -300,151 +307,6 @@ struct RateUserRequest: public Request
 struct RateUserResponse: public generic_protocol::BackwardMessage
 {
 };
-
-namespace web
-{
-
-/**************************************************
- * WEB OBJECTS
- **************************************************/
-
-struct ProductItemWithId
-{
-    id_t            product_item_id;
-    ProductItem     product_item;
-};
-
-struct ShoppingItemWithProduct
-{
-    ShoppingItem    shopping_item;
-    ProductItem     product_item;
-};
-
-struct ShoppingListWithProduct
-{
-    std::vector<ShoppingItemWithProduct>   items;
-};
-
-struct ShoppingListWithTotals
-{
-    ShoppingListWithProduct    shopping_list;
-    double          price;
-    double          weight;
-};
-
-struct OfferWithBuyer
-{
-    id_t            offer_id;
-    Offer     offer_with_state;
-    std::string     buyer_name;
-};
-
-struct OfferWithStateWithId
-{
-    id_t            offer_id;
-    OfferWithState            offer_with_state;
-};
-
-struct ShoppingRequestInfo
-{
-    id_t            order_id;
-    double          sum;
-    double          earning;
-    double          weight;
-    Address         address;
-};
-
-struct AcceptedOrderUser
-{
-    id_t            order_id;
-    basic_objects::LocalTime delivery_time;
-    Order           order;
-    double          sum;
-    std::string     buyer_name;
-};
-
-struct AcceptedOrderBuyer
-{
-    id_t            order_id;
-    basic_objects::LocalTime delivery_time;
-    Order           order;
-    double          sum;
-    double          earning;
-    double          weight;
-};
-
-struct DashScreenUser
-{
-    basic_objects::LocalTime        current_time;
-
-    std::vector<OfferWithBuyer> offer_with_states;
-    std::vector<AcceptedOrderUser>      orders;
-};
-
-struct DashScreenBuyer
-{
-    basic_objects::LocalTime        current_time;
-
-    std::vector<OfferWithStateWithId>             offer_with_states;
-    std::vector<AcceptedOrderBuyer>   orders;
-};
-
-/**************************************************
- * WEB REQUESTS
- **************************************************/
-
-struct GetProductItemListRequest: public Request
-{
-};
-
-struct GetProductItemListResponse: public generic_protocol::BackwardMessage
-{
-    std::vector<ProductItemWithId>  product_items;
-};
-
-struct GetShoppingRequestInfoRequest: public Request
-{
-    id_t            offer_id;
-};
-
-struct GetShoppingRequestInfoResponse: public generic_protocol::BackwardMessage
-{
-    std::vector<ShoppingRequestInfo>    requests;
-};
-
-struct GetShoppingListWithTotalsRequest: public Request
-{
-    id_t            shopping_list_id;
-};
-
-struct GetShoppingListWithTotalsResponse: public generic_protocol::BackwardMessage
-{
-    ShoppingListWithTotals  shopping_list;
-};
-
-struct GetDashScreenUserRequest: public Request
-{
-    id_t            user_id;
-    GeoPosition     position;
-};
-
-struct GetDashScreenUserResponse: public generic_protocol::BackwardMessage
-{
-    DashScreenUser  dash_screen;
-};
-
-struct GetDashScreenBuyerRequest: public Request
-{
-    id_t            user_id;
-    GeoPosition     position;
-};
-
-struct GetDashScreenBuyerResponse: public generic_protocol::BackwardMessage
-{
-    DashScreenBuyer   dash_screen;
-};
-
-} // namespace web
 
 } // namespace lieferbay_protocol
 
---
+++ protocol_web.h
@@ -0,0 +1,178 @@
+/*
+
+Lieferbay Protocol messages.
+
+Copyright (C) 2019 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 12386 $ $Date:: 2019-11-16 #$ $Author: serge $
+
+#ifndef LIB_LIEFERBAY_PROTOCOL__PROTOCOL_WEB_H
+#define LIB_LIEFERBAY_PROTOCOL__PROTOCOL_WEB_H
+
+#include "protocol.h"           // ShoppingItem
+
+namespace lieferbay_protocol {
+
+namespace web
+{
+
+/**************************************************
+ * WEB OBJECTS
+ **************************************************/
+
+struct ProductItemWithId
+{
+    id_t            product_item_id;
+    ProductItem     product_item;
+};
+
+struct ShoppingItemWithProduct
+{
+    ShoppingItem    shopping_item;
+    ProductItem     product_item;
+};
+
+struct ShoppingListWithProduct
+{
+    std::vector<ShoppingItemWithProduct>   items;
+};
+
+struct ShoppingListWithTotals
+{
+    ShoppingListWithProduct    shopping_list;
+    double          price;
+    double          weight;
+};
+
+struct OfferWithBuyer
+{
+    id_t            offer_id;
+    Offer     offer_with_state;
+    std::string     buyer_name;
+};
+
+struct OfferWithStateWithId
+{
+    id_t            offer_id;
+    OfferWithState            offer_with_state;
+};
+
+struct ShoppingRequestInfo
+{
+    id_t            order_id;
+    double          sum;
+    double          earning;
+    double          weight;
+    Address         address;
+};
+
+struct AcceptedOrderUser
+{
+    id_t            order_id;
+    basic_objects::LocalTime delivery_time;
+    Order           order;
+    double          sum;
+    std::string     buyer_name;
+};
+
+struct AcceptedOrderBuyer
+{
+    id_t            order_id;
+    basic_objects::LocalTime delivery_time;
+    Order           order;
+    double          sum;
+    double          earning;
+    double          weight;
+};
+
+struct DashScreenUser
+{
+    basic_objects::LocalTime        current_time;
+
+    std::vector<OfferWithBuyer> offer_with_states;
+    std::vector<AcceptedOrderUser>      orders;
+};
+
+struct DashScreenBuyer
+{
+    basic_objects::LocalTime        current_time;
+
+    std::vector<OfferWithStateWithId>             offer_with_states;
+    std::vector<AcceptedOrderBuyer>   orders;
+};
+
+/**************************************************
+ * WEB REQUESTS
+ **************************************************/
+
+struct GetProductItemListRequest: public Request
+{
+};
+
+struct GetProductItemListResponse: public generic_protocol::BackwardMessage
+{
+    std::vector<ProductItemWithId>  product_items;
+};
+
+struct GetShoppingRequestInfoRequest: public Request
+{
+    id_t            offer_id;
+};
+
+struct GetShoppingRequestInfoResponse: public generic_protocol::BackwardMessage
+{
+    std::vector<ShoppingRequestInfo>    requests;
+};
+
+struct GetShoppingListWithTotalsRequest: public Request
+{
+    id_t            shopping_list_id;
+};
+
+struct GetShoppingListWithTotalsResponse: public generic_protocol::BackwardMessage
+{
+    ShoppingListWithTotals  shopping_list;
+};
+
+struct GetDashScreenUserRequest: public Request
+{
+    id_t            user_id;
+    GeoPosition     position;
+};
+
+struct GetDashScreenUserResponse: public generic_protocol::BackwardMessage
+{
+    DashScreenUser  dash_screen;
+};
+
+struct GetDashScreenBuyerRequest: public Request
+{
+    id_t            user_id;
+    GeoPosition     position;
+};
+
+struct GetDashScreenBuyerResponse: public generic_protocol::BackwardMessage
+{
+    DashScreenBuyer   dash_screen;
+};
+
+} // namespace web
+
+} // namespace lieferbay_protocol
+
+#endif // LIB_LIEFERBAY_PROTOCOL__PROTOCOL_WEB_H
